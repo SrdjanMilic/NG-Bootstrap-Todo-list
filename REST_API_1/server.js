7e286ada16ebe883f7694d485e7aa3fdbf20e0c4
Srdjan Milic <srdjanrmilic@gmail.com>
2020-07-20 13:07:26 +0200

    move sensitive info to .env file

--- REST_API_1/server.js
+++ REST_API_1/server.js
@@ -5,8 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const db = mongoose.connection;
-const todosRouter = require('./controler');
-const PORT = 3000;
+const todosRouter = require('./controller');
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true  });
 db.on('error', (error) => console.error(error));
@@ -19,4 +18,4 @@ app.use(cors());
 
 app.use('/api/v1/todoListDB', todosRouter);
 
-app.listen(PORT, () => console.log('server is up at port: ' + PORT));
+app.listen(process.env.PORT, () => console.log('server running at port: ' + process.env.PORT));
